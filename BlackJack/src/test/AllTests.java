ba340aededa2042ffe7fc111edfa36a21d37e25c
stephen <stephen.gentles@gmail.com>
2016-04-12 22:47:25 -0500

    Refactor players test class

--- BlackJack/src/test/AllTests.java
+++ BlackJack/src/test/AllTests.java
@@ -11,7 +11,7 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 
 @RunWith(Suite.class)
-@SuiteClasses({ PlayerCanAddCardTest.class })
+@SuiteClasses({ PlayerTests.class })
 public class AllTests {
 
 }
--- BlackJack/src/test/PlayerTests.java
+++ BlackJack/src/test/PlayerTests.java
@@ -13,10 +13,10 @@ import org.junit.Test;
  *
  */
 
-public class PlayerCanAddCardTest {
+public class PlayerTests {
 
 	@Test
-	public void PlayerCanAddCard() {
+	public void player_can_add_card() {
 		//Arrange
 		Player player = new Player("TestPlayer",1);
 		Deck theDeck = new Deck(1, true);
@@ -27,5 +27,18 @@ public class PlayerCanAddCardTest {
 		//Assert
 		assertTrue(result);
 	}
+	
+	@Test
+	public void player_has_valid_string_name() {
+				//Arrange
+				Player player = new Player("TestPlayer",1);
+								
+				//Act
+				String result = player.getPlayerName();
+				
+				//Assert
+				assertEquals("TestPlayer", result);
+	}
+
 
 }
